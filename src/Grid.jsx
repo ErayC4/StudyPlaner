d07125d3bf0afe765c328137a0a6b4deef66d9bf
ErayC4 <eray04.coskun@gmail.com>
2024-05-11 14:27:54 +0200

    adding time and calculating height of timeblocks

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,34 +1,104 @@
-import React from 'react'
+import React from "react";
 
 function Grid() {
-    const daysOfWeek = [
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday'
-      ];
+  const daysOfWeek = [
+    { name: "Monday" },
+    { name: "Tuesday" },
+    { name: "Wednesday" },
+    { name: "Thursday" },
+    { name: "Friday" },
+    { name: "Saturday" },
+    { name: "Sunday" },
+  ];
 
-    const stundenArray = Array.from({ length: 48 }, (_, index) => index);
+  const timeBlocks = [
+    {
+      name: "THI",
+      isActiveOn: "Wednesday",
+      startingTime: "08:25",
+      endingTime: "09:25",
+    },
+    {
+      name: "THI",
+      isActiveOn: "Monday",
+      startingTime: "01:25",
+      endingTime: "09:25",
+    },
+    
+    {
+      name: "THI",
+      isActiveOn: "Thursday",
+      startingTime: "05:25",
+      endingTime: "19:25",
+    },
+    
+  ];
+  function calculateTime(startingTime, endingTime) {
+    const endingTimeParts = endingTime.split(":");
+    const startingTimeParts = startingTime.split(":");
 
+    // Umwandeln der Teile in Integer
+    const endingHours = parseInt(endingTimeParts[0]);
+    const endingMinutes = parseInt(endingTimeParts[1]);
+    const startingHours = parseInt(startingTimeParts[0]);
+    const startingMinutes = parseInt(startingTimeParts[1]);
+    const timeLength =
+      endingHours * 60 + endingMinutes - (startingHours * 60 + startingMinutes);
+    return timeLength;
+  }
+
+  const stundenArray = Array.from({ length: 48 }, (_, index) => index);
   return (
-    <div className='px-16'>
-        <div className='grid grid-cols-7'>
-        {daysOfWeek.map((day, dayIndex)=>(
-            <div key={dayIndex}>
-            <p key ={dayIndex}>{day}</p>
-            {stundenArray.map((index) => (
-        <div key={index} className={`w-full h-8 border-r border-gray-600 ${index % 2 != 0 ? "bg-transparent" : "bg-gray-300 border-b border-gray-600"}`}></div>
-      ))}
+    <div className="px-16">
+      <div className="grid grid-cols-7"></div>
+      <div className="grid grid-cols-7">
+        {daysOfWeek.map((day, dayIndex) => (
+          <div key={dayIndex}>
+            <p
+              className="border-b border-r p-2 text-xl border-gray-600"
+              key={dayIndex}
+            >
+              {day.name}
+            </p>
+            <div className="absolute z-10">
+              {timeBlocks.map((timeBlock, index) => (
+                <div key={index}>
+                  {day.name == timeBlock.isActiveOn ? (
+                    <div
+                      style={{
+                        backgroundColor: "#34D399",
+                        height: `${calculateTime(
+                          timeBlock.startingTime,
+                          timeBlock.endingTime
+                        )}px`,
+                      }}
+                    >
+                      true
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              ))}
             </div>
+
+            <div className="relative">
+              {stundenArray.map((index) => (
+                <div
+                  key={index}
+                  className={`w-full h-[30px] border-r border-gray-600 ${
+                    index % 2 == 0
+                      ? "bg-transparent"
+                      : "bg-gray-300 border-b border-gray-600"
+                  }`}
+                ></div>
+              ))}
+            </div>
+          </div>
         ))}
-        </div>
-        
-      
+      </div>
     </div>
   );
 }
 
-export default Grid
+export default Grid;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Grid from './Grid.jsx'
+import AddBlock from './AddBlock.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AddBlock />
     <Grid />
   </React.StrictMode>,
 )
